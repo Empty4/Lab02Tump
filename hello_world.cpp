0e9f28507736bd25f3dace31ae4cba89ffc4fa8e
Empty4 <81508434+Empty4@users.noreply.github.com>
2025-09-14 21:11:18 +0300

    Update hello_world.cpp
    Changed comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,7 +5,7 @@ int main() {
     std::string name;
     std::cout << "Enter your name: ";
     std::getline(std::cin, name);
-    std::cout << "Hello " << (name.empty() ? "World" : "world from " + name) << "!" << std::endl; //? mean if constrution
+    std::cout << "Hello " << (name.empty() ? "World" : "world from " + name) << "!" << std::endl; //? oneline if constract
     return 0;
 }
 
